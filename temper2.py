1612a0d85e64fce6a303ed22c59def1d62bc31e2
Nick Hughes <nhughes@freelancer.com>
2018-05-16 21:20:48 +1000

    Improvements

--- temper2.py
+++ temper2.py
@@ -7,35 +7,67 @@ class Temper2:
 
     vendor_id = 0x413D
     product_id = 0x2107
+    name_query = [0x01, 0x86, 0xff, 0x01, 0x00, 0x00, 0x00, 0x00]
+    temp_query = [0x01, 0x80, 0x33, 0x01, 0x00, 0x00, 0x00, 0x00]
+    max_open_tries = 5
 
     def __init__(self):
-        self.h = hid.device(self.vendor_id, self.product_id)
-        self.h.open(self.vendor_id, self.product_id)
+        self.h = hid.device()
+        is_open = False
+        tries = 0
+        while not is_open:
+            try:
+                tries += 1
+                self.h.open(self.vendor_id, self.product_id)
+            except:
+                if tries >= self.max_open_tries:
+                    raise IOError('could not open device')
+                time.sleep(0.1)
+                continue
+            is_open = True
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, tb):
+        self.h.close()
 
     def get_name(self):
-        self.h.write([0x01, 0x86, 0xff, 0x01, 0x00, 0x00, 0x00, 0x00])
-        resp1 = self.h.read(8)
-        resp2 = self.h.read(8)
-        resp1_chars = ''.join([chr(r) for r in resp1])
-        resp2_chars = ''.join([chr(r) for r in resp2])
+        self.write(self.name_query)
+        resp1 = self.read(8)
+        resp2 = self.read(8)
+        resp1_chars = self._read_string(resp1)
+        resp2_chars = self._read_string(resp2)
         return resp1_chars + resp2_chars
 
     def get_temp(self):
-        self.h.write([0x01, 0x80, 0x33, 0x01, 0x00, 0x00, 0x00, 0x00])
-        resp1 = self.h.read(8)
-        # FIXME: This second read will hang if there's no external
-        #        temperature probe connected
-        resp2 = self.h.read(8)
-        int_temp = ((resp1[2] << 8) + resp1[3]) / 100
-        ext_temp = ((resp2[2] << 8) + resp2[3]) / 100
+        self.write(self.temp_query)
+        resp1 = self.read(8)
+        resp2 = self.read(8)
+        int_temp = self._read_temp(resp1)
+        ext_temp = self._read_temp(resp2)
         return int_temp, ext_temp
+
+    def write(self, data):
+        self.h.write(data)
+
+    def read(self, size):
+        return self.h.read(size)
+
+    @staticmethod
+    def _read_string(b):
+        return ''.join([chr(r) for r in b])
+
+    @staticmethod
+    def _read_temp(b):
+        return ((b[2] << 8) + b[3]) / 100
 
 
 if __name__ == '__main__':
-    t = Temper2()
-    name = t.get_name()
-    print(name)
-    while True:
-        int_temp, ext_temp = t.get_temp()
-        print(int_temp, ext_temp)
-        time.sleep(1)
+    with Temper2() as t:
+        name = t.get_name()
+        print(name)
+        while True:
+            int_temp, ext_temp = t.get_temp()
+            print(int_temp, ext_temp)
+            time.sleep(1)
